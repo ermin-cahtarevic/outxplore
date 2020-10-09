d06b3112967741edd89329be61739024a4f95d50
Ermin Cahtarevic <erminc@windowslive.com>
2020-10-09 02:51:10 +0200

    Add styling for sign up page

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import './main.css';
 
 const Main = () => {
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, []);
-
-  const handleScroll = () => {
-    if (window.scrollY > 20) {
-      document.querySelector('.navbar').classList.add('nav-scroll');
-    } else {
-      document.querySelector('.navbar').classList.remove('nav-scroll');
-    }
-  }
-
   return (
     <main className="main">
       <div className="main-top"></div>
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './navbar.css';
 
 const Navbar = () => {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 20) {
+      document.querySelector('.navbar').classList.add('nav-scroll');
+    } else {
+      document.querySelector('.navbar').classList.remove('nav-scroll');
+    }
+  }
+
   return (
     <nav className="navbar">
       <div className="nav-wrap">
-        <div className="logo"><Link to="/">Logo</Link></div>
+        <div className="logo"><Link to="/">outxplore</Link></div>
         <div>
           <ul className="nav-list">
             <li><Link>Become a Host</Link></li>
