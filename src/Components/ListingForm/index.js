def7407e877d7f54c7a58c9180f713804318613a
Ermin Cahtarevic <erminc@windowslive.com>
2020-10-22 00:26:39 +0200

    Update Listing form with activity type and max guest num fields

--- src/Components/ListingForm/index.js
+++ src/Components/ListingForm/index.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import Place from 'react-algolia-places';
 
 import './listing-form.css';
+import { activities } from '../helper';
 
 const ListingForm = () => {
   const initialState = {
     title: '',
     description: '',
+    activityType: '',
+    guestMaxNum: 1,
     photos: [],
     price: 0,
   };
@@ -14,7 +17,7 @@ const ListingForm = () => {
   const [formInput, setFormInput] = useState(initialState);
   const [locationInput, setLocationInput] = useState('');
 
-  const { title, description, photos, price } = formInput;
+  const { title, description, activityType, guestMaxNum, photos, price } = formInput;
 
   const handleChange = e => {
     setFormInput({
@@ -44,8 +47,10 @@ const ListingForm = () => {
     const data = {
       lisitng: {
         ...formInput,
-        photos: photoData,
+        activity_type: activityType,
+        guest_max_num: parseInt(guestMaxNum),
         location: locationInput,
+        photos: photoData,
       },
     };
 
@@ -60,7 +65,7 @@ const ListingForm = () => {
         <form onSubmit={handleSubmit}>
           <div>
             <h4>Title</h4>
-            <p>Add in a title for your activity lisitng.</p>
+            <p>Add in a title for your activity lisitng. <span className="red-star">*</span></p>
             <input
               type="text"
               name="title"
@@ -71,7 +76,7 @@ const ListingForm = () => {
           </div>
           <div>
             <h4>Description</h4>
-            <p>Describe the activity in as much detail as you can.</p>
+            <p>Describe the activity in as much detail as you can. <span className="red-star">*</span></p>
             <textarea
               name="description"
               value={description}
@@ -80,8 +85,40 @@ const ListingForm = () => {
             />
           </div>
           <div>
+            <h4>Activity type</h4>
+            <p>Choose the type of activity you will be hosting. <span className="red-star">*</span></p>
+            <select
+              className="host-form-select"
+              onChange={handleChange}
+              placeholder="Select the activities"
+              required
+              name="activityType"
+              value={activityType}
+            >
+              {
+                activities.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))
+              }
+            </select>
+          </div>
+          <div>
+            <h4>Maximum number of guests</h4>
+            <p>What is the maximum number of guests you would take on? <span className="red-star">*</span></p>
+            <input
+              className="host-form-number-input"
+              type="number"
+              onChange={handleChange}
+              min={1}
+              max={25}
+              required
+              name="guestMaxNum"
+              value={guestMaxNum}
+            />
+          </div>
+          <div>
             <h4>Location</h4>
-            <p>Where will the activity be held?</p>
+            <p>Where will the activity be held? <span className="red-star">*</span></p>
             <Place
               onChange={e => setLocationInput(e.suggestion.value)}
               value={locationInput}
@@ -90,7 +127,7 @@ const ListingForm = () => {
           </div>
           <div>
             <h4>Photos</h4>
-            <p>Add at least two photos of the activity and the location. <span>(min 2 - max 5)</span></p>
+            <p>Add at least two photos of the activity and the location. <span>(min 2 - max 5)</span> <span className="red-star">*</span></p>
             <input
               type="file"
               multiple={true}
@@ -100,7 +137,7 @@ const ListingForm = () => {
           </div>
           <div>
             <h4>Price</h4>
-            <p>How much will the activity cost per person?</p>
+            <p>How much will the activity cost per person? <span className="red-star">*</span></p>
             <div className="activity-listing-price-input">
               $
               <input
@@ -113,6 +150,8 @@ const ListingForm = () => {
               />
             </div>
           </div>
+          <span><span className="red-star">*</span> - Required fields</span>
+
           <button type="submit" className="activity-listing-submit-btn">Submit</button>
         </form>
       </div>
